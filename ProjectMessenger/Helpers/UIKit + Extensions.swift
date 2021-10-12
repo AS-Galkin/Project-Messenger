6018b99fd0c946f235593d14ef7d8da396eb3ff7
as-galkin <demotivatoris@gmail.com>
2021-10-12 15:33:42 +0300

    - Second step of TDD. Convenience init is implemented. Test is green.

---
+++ ProjectMessenger/Helpers/UIKit + Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  UIKit + Extensions.swift
+//  ProjectMessenger
+//
+//  Created by Александр Галкин on 12.10.2021.
+//
+
+import UIKit
+
+extension UIButton {
+    convenience init(title: String,
+                     titleColor: UIColor,
+                     backgroundColor: UIColor,
+                     font: UIFont? = .avenir20,
+                     isShadow: Bool = false,
+                     cornerRadius: CGFloat = 4) {
+        self.init(type: .system)
+        
+        self.setTitle(title, for: .normal)
+        self.setTitleColor(titleColor, for: .normal)
+        self.backgroundColor = backgroundColor
+        self.titleLabel?.font = font
+        self.layer.cornerRadius = cornerRadius
+        
+        if isShadow {
+            self.layer.shadowColor = UIColor.black.cgColor
+            self.layer.shadowRadius = 4
+            self.layer.shadowOpacity = 0.5
+            self.layer.shadowOffset = CGSize(width: 0, height: 4)
+        }
+    }
+}
+
+extension UIFont {
+    static let avenir20 = UIFont(name: "Avenir", size: 20.0)
+}
